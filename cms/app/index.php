7d764f6fce43312293a40886650c14ed3245476f
dyong525 <zhpei918>
2013-07-26 13:56:47 +0800

    r

--- cms/app/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'www' .DIRECTORY_SEPARATOR . 'index.php';
-
-?>
--- cms/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'app' .DIRECTORY_SEPARATOR . 'index.php';
-
-?>
